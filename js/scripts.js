8571537ebba5f03770a0ae1bb04f85748399911c
Shaka Willock <shakawillock@gmail.com>
2022-01-10 14:05:56 -0500

    Get reference for bookTitle, bookAuthor, bookPages, submitBtn Add addEventListener to submitBtn, check for empty form inputs. Create new book instance and pass to addBookToLibrary. Push new book to myLibrary.

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,11 @@
 const modal = document.querySelector('#my-modal');
 const modalBtn = document.querySelector('#modal-btn');
 const closeModal = document.querySelector('.close');
+const title = document.querySelector('#book-title');
+const author = document.querySelector('#book-author');
+const pages = document.querySelector('#book-pages');
+const read = document.querySelector('#book-read');
+const submitBtn = document.querySelector('.submit-btn');
 
 modalBtn.addEventListener('click', () => {
   modal.style.display = 'block';
@@ -16,6 +21,29 @@ window.addEventListener('click', e => {
   }
 });
 
+submitBtn.addEventListener('click', e => {
+  e.preventDefault();
+
+  const bookTitle = title.value;
+  const bookAuthor = author.value;
+  const bookPages = Number(pages.value);
+  let bookRead;
+
+  checkBookInput(bookTitle, bookAuthor, bookPages);
+
+  if (read.checked) {
+    bookRead = 'Read'
+  } else {
+    bookRead = 'Not Read'
+  }
+
+  const book = new Book(bookTitle, bookAuthor, bookPages, bookRead);
+  addBookToLibrary(book);
+
+  modal.style.display = "none";
+});
+
+
 let myLibrary = [];
 
 function Book(title, author, pages, read) {
@@ -25,6 +53,14 @@ function Book(title, author, pages, read) {
   this.read = read;
 }
 
-function addBookToLibrary() {
+function addBookToLibrary(book) {
+  myLibrary.push(book);
+  console.log(myLibrary);
+}
 
+function checkBookInput(title, author, pages) {
+  if (title === '' || author === '' || pages === '') {
+    alert('Please, fill in all the fields');
+    modal.style.display = "none";
+  }
 }
