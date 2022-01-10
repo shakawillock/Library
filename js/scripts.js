3f888129c9ed2dc4e5eebdfefaa6be6ce5f0efe5
Shaka Willock <shakawillock@gmail.com>
2022-01-10 13:09:19 -0500

    Add modal, styles and functionality

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,30 @@
+const modal = document.querySelector('#my-modal');
+const modalBtn = document.querySelector('#modal-btn');
+const closeModal = document.querySelector('.close');
+
+modalBtn.addEventListener('click', () => {
+  modal.style.display = 'block';
+});
+
+closeModal.addEventListener('click', () => {
+  modal.style.display = "none";
+});
+
+window.addEventListener('click', e => {
+  if (e.target === modal) {
+    modal.style.display = "none";
+  }
+});
+
 let myLibrary = [];
 
-function Book() {
-
+function Book(title, author, pages, read) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
 }
 
 function addBookToLibrary() {
-  
+
 }
