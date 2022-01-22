418786c0cb6c301e78232196ddd5179eb701727e
Shaka Willock <shakawillock@gmail.com>
2022-01-22 12:34:02 -0500

    Create Card Object
    - Change deleteBook to deleteCard and clearInput to clear
    - Move createCard, createCardTitle, createCardBody, createCardText,
    createReadBtn, createDeleteBtn, changeReadBtnColor, changeReadStatus,
    and deleteCard functions to Card object
    - Move clear function to inputField object

--- js/scripts.js
+++ js/scripts.js
@@ -2,6 +2,15 @@ const modal = document.querySelector('#my-modal');
 const container = document.querySelector('.container');
 let myLibrary = JSON.parse(localStorage.getItem('books')) || [];
 
+class Book {
+  constructor(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+  }
+}
+
 const button = (() => {
   const modalBtn = document.querySelector('#modal-btn');
   const closeBtn = document.querySelector('.close');
@@ -20,11 +29,19 @@ const inputField = (() => {
   const pages = document.querySelector('#book-pages');
   const read = document.querySelector('#book-read');
 
+  function clear() {
+    inputField.title.value = '';
+    inputField.author.value = '';
+    inputField.pages.value = '';
+    inputField.read.checked = false;
+  }
+
   return {
     title,
     author,
     pages,
-    read
+    read,
+    clear
   }
 })();
 
@@ -36,15 +53,6 @@ button.closeBtn.addEventListener('click', closeModal);
 
 function closeModal() {
   modal.style.display = 'none';
-}
-
-class Book {
-  constructor(title, author, pages, read) {
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.read = read;
-  }
 }
 
 button.submitBtn.addEventListener('click', e => {
@@ -69,7 +77,7 @@ button.submitBtn.addEventListener('click', e => {
   addBookToLibrary(book);
 
   closeModal()
-  clearInput();
+  inputField.clear();
 });
 
 function checkBookInput(title, author, pages) {
@@ -87,19 +95,19 @@ function addBookToLibrary(book) {
 }
 
 function displayBook(book) {
-  const card = createCard();
-  const cardBody = createCardBody();
-  const cardTitle = createCardTitle();
-  const paras = createCardText();
-  const readBtn = createReadButton();
-  const deleteBtn = createDeleteButton();
+  const card = Card.createCard();
+  const cardBody = Card.createCardBody();
+  const cardTitle = Card.createCardTitle();
+  const paras = Card.createCardText();
+  const readBtn = Card.createReadButton();
+  const deleteBtn = Card.createDeleteButton();
 
   const titleOfBook = document.createTextNode(book.title);
   const authorOfBook = document.createTextNode(book.author);
   const pagesOfBook = document.createTextNode(book.pages + 'pg');
   const bookRead = document.createTextNode(book.read);
 
-  changeReadBtnColor(readBtn, bookRead);
+  Card.changeReadBtnColor(readBtn, bookRead);
 
   readBtn.appendChild(bookRead);
   paras[0].appendChild(authorOfBook);
@@ -109,89 +117,96 @@ function displayBook(book) {
   card.appendChild(cardBody);
   container.appendChild(card);
 
-  readBtn.addEventListener('click', changeReadStatus);
-  deleteBtn.addEventListener('click', deleteBook);
+  readBtn.addEventListener('click', Card.changeReadStatus);
+  deleteBtn.addEventListener('click', Card.deleteCard);
 }
 
-function createCard() {
-  const card = document.createElement('div');
-  card.classList.add('card');
-  return card;
-}
-
-function createCardTitle() {
-  const cardTitle = document.createElement('h3');
-  return cardTitle;
-}
-
-function createCardBody() {
-  const cardBody = document.createElement('div');
-  cardBody.classList.add('card__body');
-  return cardBody
-}
-
-function createCardText() {
-  const para = document.createElement('p');
-  const para2 = document.createElement('p');
-  return [para, para2];
-}
-
-function createReadButton() {
-  const button = document.createElement('button');
-  button.classList.add('card__button')
-  return button;
-}
-
-function createDeleteButton() {
-  const button = document.createElement('button');
-  button.classList.add('card__button', 'card__button--delete');
-  const deleteBtnText = document.createTextNode('Remove');
-  button.append(deleteBtnText);
-  return button;
-}
-
-function changeReadBtnColor(readBtn, bookRead) {
-  if (bookRead.textContent === 'Read') {
-    readBtn.classList.add('card__button--green');
-  } else  {
-    readBtn.classList.add('card__button--red');
+const Card = (() => {
+  function createCard() {
+    const card = document.createElement('div');
+    card.classList.add('card');
+    return card;
   }
-}
+  
+  function createCardTitle() {
+    const cardTitle = document.createElement('h3');
+    return cardTitle;
+  }
+  
+  function createCardBody() {
+    const cardBody = document.createElement('div');
+    cardBody.classList.add('card__body');
+    return cardBody
+  }
+  
+  function createCardText() {
+    const para = document.createElement('p');
+    const para2 = document.createElement('p');
+    return [para, para2];
+  }
+  
+  function createReadButton() {
+    const button = document.createElement('button');
+    button.classList.add('card__button')
+    return button;
+  }
+  
+  function createDeleteButton() {
+    const button = document.createElement('button');
+    button.classList.add('card__button', 'card__button--delete');
+    const deleteBtnText = document.createTextNode('Remove');
+    button.append(deleteBtnText);
+    return button;
+  }
 
-function deleteBook(e) {
-  const cardBody = e.target.parentElement;
-  const card = cardBody.parentElement;
-  const bookTitle = cardBody.firstElementChild.textContent;
-
-  for (let i = 0; i < myLibrary.length; i++) {
-    if (bookTitle === myLibrary[i].title) {
-      myLibrary.splice(i, 1);
-      card.remove();
-      removeLocalStorage(i);
+  function changeReadBtnColor(readBtn, bookRead) {
+    if (bookRead.textContent === 'Read') {
+      readBtn.classList.add('card__button--green');
+    } else  {
+      readBtn.classList.add('card__button--red');
     }
   }
-}
-
-function changeReadStatus(e) {
-  if (e.target.textContent === 'Read') {
-    e.target.textContent = 'Not Read';
-    e.target.classList.remove('card__button--green');
-    changeReadBtnColor(e.target, e.target);
-  } else {
-    e.target.textContent = 'Read';
-    e.target.classList.remove('card__button--red');
-    changeReadBtnColor(e.target, e.target);
+  
+  function changeReadStatus(e) {
+    if (e.target.textContent === 'Read') {
+      e.target.textContent = 'Not Read';
+      e.target.classList.remove('card__button--green');
+      changeReadBtnColor(e.target, e.target);
+    } else {
+      e.target.textContent = 'Read';
+      e.target.classList.remove('card__button--red');
+      changeReadBtnColor(e.target, e.target);
+    }
+  
+    updateLocalStorage(e);
+  }
+  
+  function deleteCard(e) {
+    const cardBody = e.target.parentElement;
+    const card = cardBody.parentElement;
+    const bookTitle = cardBody.firstElementChild.textContent;
+  
+    for (let i = 0; i < myLibrary.length; i++) {
+      if (bookTitle === myLibrary[i].title) {
+        myLibrary.splice(i, 1);
+        card.remove();
+        removeLocalStorage(i);
+      }
+    }
   }
 
-  updateLocalStorage(e);
-}
-
-function clearInput() {
-  inputField.title.value = '';
-  inputField.author.value = '';
-  inputField.pages.value = '';
-  inputField.read.checked = false;
-}
+  return {
+    createCard,
+    createCardTitle,
+    createCardBody,
+    createCardText,
+    createReadButton,
+    createDeleteButton,
+    changeReadBtnColor,
+    changeReadStatus,
+    deleteCard,
+  }
+})();
 
 window.addEventListener('click', e => {
   if (e.target === modal) {
